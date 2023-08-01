aa6cfb9c1e1ceffd2ee9fa52080c6dddcb1eaf20
Emma <emmalynes@aol.com>
2023-08-01 11:40:03 +0100

    Initial Commit

---
+++ finalprojectparts/finalprojectpartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalprojectpartsApp.swift
+//  finalprojectparts
+//
+//  Created by scholar on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct finalprojectpartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
